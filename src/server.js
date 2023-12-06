b161c9f704b72c1e59ef67c05d81e60249434828
mafiabawang <mafiabawang@gmail.com>
2023-12-06 16:00:29 +0700

    crud notes and users

---
+++ src/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+//notes 
+const notes = require('./api/notes');
+const NotesService = require('./service/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+//users
+const users = require('./api/users');
+const UsersService = require('./service/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+const init = async () => {
+    const notesService = new NotesService();
+    const usersService = new UsersService();
+
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
+    });
+
+    await server.register([
+        {
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator
+            }
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator
+            }
+        }
+    ]);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
